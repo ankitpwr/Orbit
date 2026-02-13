4c9d21c1927e8e47d4ecbf3b13f4b1ea34b795c7
ankit pwr <ankitpam321@gmail.com>
2026-02-14 00:57:13 +0530

    feat: add fetching data from db in batch in producer

--- api/src/worker/producer.ts
+++ api/src/worker/producer.ts
@@ -1,26 +1,51 @@
 import cron from "node-cron";
 import { prisma } from "../lib/prisma.js";
 import { producerClient } from "../lib/redis.js";
+const BATCH_SIZE = 2;
 
-try {
-  async function publish() {
-    const monitor = await prisma.monitor.findMany({
-      select: { id: true, url: true },
-    });
-    console.log(monitor);
-    const pipeline = producerClient.pipeline();
-    monitor.forEach((obj) => {
-      const entries = Object.entries(obj).flatMap(([k, v]) => [
-        k,
-        v == null ? "" : String(v),
-      ]);
-      pipeline.xadd("Orbit:monitors", "*", ...entries);
-    });
-
-    await pipeline.exec();
-  }
-  cron.schedule("*/2 * * * *", publish);
-} catch (error) {
-  console.log("error !");
-  console.log(error);
+interface MonitorData {
+  id: string;
+  url: string;
 }
+
+async function publish() {
+  try {
+    console.log("starting producer! ");
+    let cursorId: string | undefined = undefined;
+    while (true) {
+      const monitors: MonitorData[] = await prisma.monitor.findMany({
+        ...(cursorId ? { cursor: { id: cursorId } } : {}),
+        skip: cursorId ? 1 : 0,
+        take: BATCH_SIZE,
+        orderBy: [{ createdAt: "asc" }, { id: "asc" }],
+        select: { id: true, url: true },
+      });
+      console.log("monitors are", monitors);
+
+      if (monitors.length == 0) break;
+
+      const pipeline = producerClient.pipeline();
+      monitors.forEach((obj) => {
+        const entries = Object.entries(obj).flatMap(([k, v]) => [
+          k,
+          v == null ? "" : String(v),
+        ]);
+        pipeline.xadd("Orbit:monitors", "*", ...entries);
+      });
+      await pipeline.exec();
+      cursorId = monitors[monitors.length - 1]?.id;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+const task = cron.schedule(
+  "*/2 * * * *",
+  async () => {
+    await publish();
+  },
+  {
+    noOverlap: true,
+  },
+);
+task.start();
